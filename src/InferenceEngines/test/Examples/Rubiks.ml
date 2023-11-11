603885a143b2c00b16d51979fb07edb720552e52
Romain <romain.pipon@etu.univ-nantes.fr>
2023-11-11 21:24:12 +0100

    Add solver

--- src/InferenceEngines/test/Examples/Rubiks.ml
+++ src/InferenceEngines/test/Examples/Rubiks.ml
@@ -3,8 +3,15 @@
 type couleur = R of char | G of char | B of char | Y of char | O of char | W of char
 
 type piece = (couleur * int)
+type identifier = char
 
-type face =  UP | DOWN | LEFT | RIGHT | FRONT | BACK
+type face =
+  UP of identifier
+  | DOWN of identifier
+  | LEFT of identifier
+  | RIGHT of identifier
+  | FRONT of identifier
+  | BACK of identifier
 
 (* ###### MARK: FONCTIONS UTILES ######*)
 
@@ -41,6 +48,25 @@ let rec split (lst: 'a list) (n: int) : ('a list * 'a list) =
 
 (* ###### MARK: FONCTIONS LIE AU RUBIKS ######*)
 
+
+type r_state = {unState : piece list}
+
+let string_of_state { unState } =
+  let rec string_of_state_aux = function
+    | [] -> ""
+    | (c, _) :: tail -> (match c with
+      | R c | G c | B c | Y c | O c | W c -> String.make 1 c) ^ string_of_state_aux tail
+  in
+  string_of_state_aux unState
+
+let string_of_move = function
+  | UP pid -> Char.escaped pid ^ "U"
+  | DOWN pid -> Char.escaped pid ^ "D"
+  | LEFT pid -> Char.escaped pid ^ "L"
+  | RIGHT pid -> Char.escaped pid ^ "R"
+  | FRONT pid -> Char.escaped pid ^ "F"
+  | BACK pid -> Char.escaped pid ^ "B"
+
 let etat_final : piece list = [
   (W 'w', 1); (W 'w', 2); (Y 'w', 3); (W 'w', 4); (*0, 1, 2, 3*)
   (R 'r', 1); (R 'r', 2); (R 'r', 3); (R 'r', 4); (*4, 5, 6, 7*)
@@ -51,12 +77,12 @@ let etat_final : piece list = [
 ]
 
 let indice_rotation (face: face) : int list = match face with
-  | UP -> [0; 1; 2; 3; 7; 6; 8; 11; 13; 12; 18; 17]
-  | DOWN -> [21; 20; 23; 22; 4; 5; 9; 10; 14; 15; 19; 16]
-  | RIGHT -> [11; 8; 9; 10; 2; 1; 6; 5; 20; 23; 14; 13]
-  | LEFT -> [17; 18; 19; 16; 0; 3; 12; 15; 22; 21; 4; 7]
-  | FRONT -> [12; 13; 14; 15; 3; 2; 11; 10; 23; 22; 19; 18]
-  | BACK -> [6; 7; 4; 5; 1; 0; 17; 16; 21; 20; 9; 8]
+  | UP pid -> [0; 1; 2; 3; 7; 6; 8; 11; 13; 12; 18; 17]
+  | DOWN pid-> [21; 20; 23; 22; 4; 5; 9; 10; 14; 15; 19; 16]
+  | RIGHT pid -> [11; 8; 9; 10; 2; 1; 6; 5; 20; 23; 14; 13]
+  | LEFT pid -> [17; 18; 19; 16; 0; 3; 12; 15; 22; 21; 4; 7]
+  | FRONT pid -> [12; 13; 14; 15; 3; 2; 11; 10; 23; 22; 19; 18]
+  | BACK pid -> [6; 7; 4; 5; 1; 0; 17; 16; 21; 20; 9; 8]
 
 let rec extraire_pieces (indices: int list) (s: piece list) : piece list=
   match indices with
@@ -75,23 +101,58 @@ let rec rotation_angle (angle: int) (s: piece list) : piece list =
   | 180 -> rotation_angle 90 (rotation_angle 90 s)
   | _ -> raise (Failure "Angle invalide")
 
-let rotation (face: face) (angle: int) (s: piece list) : piece list =
-  let indices = indice_rotation face in
-  let pieces = extraire_pieces indices s in
-  let rotated_pieces = rotation_angle angle pieces in
-  let rec remplacer_pieces indices pieces s =
-    match indices, pieces with
-    | [], [] -> s
-    | i :: tail, p :: tail2 -> remplacer_pieces tail tail2 (List.mapi (fun index (c, _) -> if index = i then p else (c, index + 1)) s)
-    | _ -> raise (Failure "Erreur de rotation")
-  in
-  remplacer_pieces indices rotated_pieces s
 
 let extraire_caracteres (s : piece list) : char list =
-    List.map (fun (couleur, _) ->
-      match couleur with
-      | R c | G c | B c | Y c | W c | O c -> c
-    ) s
+  List.map (fun (couleur, _) ->
+    match couleur with
+    | R c | G c | B c | Y c | W c | O c -> c
+  ) s
+let isSolved {unState} = extraire_caracteres unState = ['w'; 'w'; 'w'; 'w'; 'r'; 'r'; 'r'; 'r'; 'b'; 'b'; 'b'; 'b'; 'g'; 'g'; 'g'; 'g'; 'y'; 'y'; 'y'; 'y'; 'o'; 'o'; 'o'; 'o']
+
+(* ###### MARK: PROBLEM ######*)
+
+module RubiksProblem = 
+( StateProblem.StateProblem
+  (struct
+    type state = r_state
+    let string_of_state  = string_of_state
+  end)
+  (struct
+    type op = face
+    let string_of_op = string_of_move
+  end)
+  (Additive.IntC)
+)
+
+
+let rotation (face: face) (angle: int) (s: r_state) : piece list =
+  let k = s.unState in
+  let indices = indice_rotation face in
+  let pieces = extraire_pieces indices k in
+  let rotated_pieces = rotation_angle angle pieces in
+  let rec remplacer_pieces indices pieces k =
+    match indices, pieces with
+    | [], [] -> k
+    | i :: tail, p :: tail2 -> remplacer_pieces tail tail2 (List.mapi (fun index (c, _) -> if index = i then p else (c, index + 1)) k)
+    | _ -> raise (Failure "Erreur de rotation")
+  in
+  remplacer_pieces indices rotated_pieces k
+
+let moves s =
+    let moves = [UP 'u'; DOWN 'd'; LEFT 'l'; RIGHT 'r'; FRONT 'f'; BACK 'b'] in
+    let angles = [90; -90; 180] in
+
+    let generate_transition_from_move angle move =
+      let end_state = rotation move angle s in
+      { RubiksProblem.startState = s;
+        operation = move;
+        endState = {unState = end_state};
+        cost = 1;
+      } in
+      let generate_transition_for angle =
+        List.map (generate_transition_from_move angle) moves 
+      in
+      List.flatten (List.map generate_transition_for angles)
 
 let pretty_print (s: piece list) : unit =
   let chars = extraire_caracteres s in
@@ -103,17 +164,32 @@ let pretty_print (s: piece list) : unit =
   Printf.printf "    %c %c\n" (i 12) (i 13);
   Printf.printf "    %c %c\n" (i 15) (i 14);
   print_newline ()
-
-let rec shuffle (s: piece list) (n: int) =
-  let moves = [UP; DOWN; LEFT; RIGHT; FRONT; BACK] in
+(* 
+let rec shuffle (s: r_state) (n: int) =
+  let moves = [UP 'u'; DOWN 'd'; LEFT 'l'; RIGHT 'r'; FRONT 'f'; BACK 'b'] in
   let rotations = [90; -90; 180] in
   if n = 0 then s
   else
     let m = List.nth moves (Random.int 6) in
     let r = List.nth rotations (Random.int 3) in
-    shuffle (rotation m r s) (n - 1)
+    shuffle (rotation m r s) (n - 1) *)
 
-let () =
-  pretty_print etat_final;
-  let rotated_cube  = shuffle etat_final 1 in
-  pretty_print rotated_cube;
+let rec shuffle (s: r_state) (n: int) =
+  let moves = [UP 'u'; DOWN 'd'; LEFT 'l'; RIGHT 'r'; FRONT 'f'; BACK 'b'] in
+  let rotations = [90; -90; 180] in
+  if n = 0 then s
+  else
+    let m = List.nth moves (Random.int 6) in
+    let r = List.nth rotations (Random.int 3) in
+    let s = rotation m r s in
+    shuffle {unState = s} (n - 1)
+
+
+
+let shuffled_cube = shuffle {unState = etat_final} 2
+
+let rubiks_flag = 1
+
+(* let () =
+    string_of_state shuffled_cube |> print_endline;
+    pretty_print shuffled_cube.unState; *)
---
+++ src/InferenceEngines/test/RubiksTest.ml
@@ -0,0 +1,37 @@
+let _ = print_endline "Début des tests."
+
+let _ = Rubiks.rubiks_flag
+
+
+let moves = Rubiks.moves Rubiks.shuffled_cube
+
+
+(* print sizes of moves *)
+let length = List.length moves
+let _ = print_endline ("Taille de la liste des mouvements :" ^ string_of_int length)
+
+
+(* print moves *)
+
+(* print moves *)
+
+
+let oracle r = 
+  if r = []
+  then print_endline "Pas de solution trouvée." 
+  else List. iter (fun s -> print_endline (Rubiks.RubiksProblem.short_solution s)) r 
+
+
+let prof = 3
+let _ = print_endline ("Débu Rubiks problem Limit (" ^string_of_int prof^ ")")
+
+module DL = DepthLimit.Solver (Rubiks.RubiksProblem)
+
+let limit_rubiks n p = DL.solver n Rubiks.moves  Rubiks.isSolved p
+
+let res_l = limit_rubiks prof Rubiks.shuffled_cube
+
+let _ = oracle res_l
+
+let _ = print_newline ()
+
